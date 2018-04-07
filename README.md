bff4ba65f72d4518fc8c5ff9ab60dc5a6580de69
Tagir Faizullin <demoflava@gmail.com>
2018-04-07 23:16:01 +0500

    Our Unity Project

--- README.md
+++
@@ -1,2 +0,0 @@
-# ION
-ION is a unique AR based educational program and app for learning basic school program using 3d models in AR. 
---
+++ Something ION/Assets/SwipeManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SwipeDirection {
+	None = 0,
+	Left = 1,
+	Right = 2,
+}
+
+public class SwipeManager : MonoBehaviour {
+	private static SwipeManager instance;
+	private static SwipeManager Instance{get{return instance;}}
+
+	public GameObject SideBar;
+	private Animator Anim;
+	public GameObject Panel;
+
+
+	public SwipeDirection Direction{ set; get;}
+	private Vector3 touchPosition;
+	private float SwipeResistanceX = 70f;
+
+	private void Start(){
+		instance = this;
+		Anim = SideBar.GetComponent<Animator>();
+	}
+		
+	private void Update ()
+	{
+		Direction = SwipeDirection.None;
+		if (Input.GetMouseButtonDown (0)) {
+			touchPosition = Input.mousePosition;
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			Vector2 deltaSwipe = touchPosition - Input.mousePosition;
+			if (Mathf.Abs (deltaSwipe.x) > SwipeResistanceX) {
+				//Свайп здесь начался на X
+				Direction |= (deltaSwipe.x < 0) ? SwipeDirection.Right : SwipeDirection.Left;
+				if (Direction == SwipeDirection.Right) {
+					Anim.SetBool ("SideOn", true);
+					Anim.SetBool ("SideOff", false);
+					Panel.SetActive (true);
+
+				} else if (Direction == SwipeDirection.Left) {
+					Anim.SetBool ("SideOff", true);
+					Anim.SetBool ("SideOn", false);
+					Panel.SetActive (false);
+				}
+		
+			}
+		}
+	}
+
+
+}
+
